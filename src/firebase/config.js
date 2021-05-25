7cf3c497d6847e011624e792deaf37458e5f9f0e
copihuexnegro <fmicheam@gmail.com>
2021-05-25 01:31:57 -0400

    prueba

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,17 +1,17 @@
-// const firebaseConfig={
+const firebaseConfig={
 
    
-//         apiKey: "AIzaSyBBsb-VN5Jx93ZLFg3ASGrXGAlol6JQcrA",
-//         authDomain: "crispaper-30459.firebaseapp.com",
-//         projectId: "crispaper-30459",
-//         storageBucket: "crispaper-30459.appspot.com",
-//         messagingSenderId: "1097049182617",
-//         appId: "1:1097049182617:web:0fae7692cf4bd5c43a0308",
-//         measurementId: "G-7GNKNR5G1Z"
+        // apiKey: "AIzaSyBBsb-VN5Jx93ZLFg3ASGrXGAlol6JQcrA",
+        // authDomain: "crispaper-30459.firebaseapp.com",
+        // projectId: "crispaper-30459",
+        // storageBucket: "crispaper-30459.appspot.com",
+        // messagingSenderId: "1097049182617",
+        // appId: "1:1097049182617:web:0fae7692cf4bd5c43a0308",
+        // measurementId: "G-7GNKNR5G1Z"
       
 
 
-// }
+}
 
 // const firebaseConfig={
 
