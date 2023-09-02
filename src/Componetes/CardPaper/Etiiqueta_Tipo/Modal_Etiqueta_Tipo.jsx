bcc2e7446cf1253e652aced2f0df9113c9d75c0c
fernando.michea@brik.cl <fernando.michea@brik.cl>
2023-09-02 17:36:05 -0400

    styled(modalTipo):esquema html y estilos modal tipo

--- src/Componetes/CardPaper/Etiiqueta_Tipo/Modal_Etiqueta_Tipo.jsx
+++ src/Componetes/CardPaper/Etiiqueta_Tipo/Modal_Etiqueta_Tipo.jsx
@@ -1,24 +1,15 @@
 import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Modal from '@material-ui/core/Modal';
-import { TextField, Box,Button } from '@material-ui/core';
-
-function rand() {
-    return Math.round(Math.random() * 20) - 10;
-}
-
-function getModalStyle() {
-    const top = 50 + rand();
-    const left = 50 + rand();
-
-    return {
-        top: `${top}%`,
-        left: `${left}%`,
-        transform: `translate(-${top}%, -${left}%)`,
-    };
-}
+import { TextField, Box,Button, Typography } from '@material-ui/core';
+import { Icon } from '@iconify/react';
 
 const useStyles = makeStyles((theme) => ({
+    modal:{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',  
+    },
     paper: {
         position: 'absolute',
         width: 400,
@@ -34,13 +25,20 @@ const useStyles = makeStyles((theme) => ({
     },
     marginButton:{
         margin:"20px 20px 20px 0px"
+    },
+    colorVerIcono:{
+        color:"#1e88e5",
+        cursor:"pointer",
+        margin:"5px"
+    },
+    padding:{
+        padding:"5px 0px"
     }
   
 }));
 
 const Modal_Etiqueta_Tipo = () => {
     const classes = useStyles();
-    const [modalStyle] = useState(getModalStyle); // Changed React.useState to useState
     const [open, setOpen] = useState(true); // Changed React.useState to useState
     
     const handleClose = () => {
@@ -53,21 +51,27 @@ const Modal_Etiqueta_Tipo = () => {
             onClose={handleClose}
             aria-labelledby="simple-modal-title"
             aria-describedby="simple-modal-description"
+            className={classes.modal}
         >
-            <div style={modalStyle} className={classes.paper}>
+            <div className={classes.paper}>
                 <h2 id="simple-modal-title">Tipo de Informacion</h2>
                 <Box display="flex" flexDirection="column"> {/* Changed to column */}
                     <Box display="flex">
                         <span className={classes.marginSpan}>Descripcion</span>
-                        <TextField fullWidth label="Ingrese el tipo de información" />
+                        <TextField fullWidth label="Ingrese el tipo de información" placeholder='Ej: Base de Datos' />
                     </Box>
                     <Box display="flex" flexDirection="column">
                         <Box display="flex" flexDirection="row">
                             <span className={classes.marginSpan}>Icono</span>
-                            <TextField fullWidth label="Ingrese icono" />
+                            <TextField fullWidth label="Ingrese icono" placeholder='Ej: material-symbols:database' />
                         </Box>
-                        <a hrer="#">
-                            Ver iconos
+                        
+                        <a className={classes.colorVerIcono} href="https://icon-sets.iconify.design/" target='_blank'>
+                            <Typography align='right'>
+                                    <Icon icon="octicon:question-16" />               
+                                    <span className={classes.padding}> Ver iconos</span>
+                            </Typography>
+                            
                         </a>
 
                     </Box>
