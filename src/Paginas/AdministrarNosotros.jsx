fdff67cd9ad451844553881c9a9efa96572df2b7
fernando.michea@brik.cl <fernando.michea@brik.cl>
2023-09-20 16:40:12 -0300

    otros cambios

--- src/Paginas/AdministrarNosotros.jsx
+++ src/Paginas/AdministrarNosotros.jsx
@@ -1,53 +1,53 @@
-import React,{useState,useEffect} from 'react'
-import { Typography, makeStyles,Grid,TextField ,Button,CircularProgress} from '@material-ui/core'
-import TextoEditor  from "../Componetes/TextoEditor.jsx"
+import React, { useState, useEffect } from 'react'
+import { Typography, makeStyles, Grid, TextField, Button, CircularProgress } from '@material-ui/core'
+import TextoEditor from "../Componetes/TextoEditor.jsx"
 import { EditorState } from 'draft-js';
 import Firebase from "../../src/firebase/firebase"
 import draftToHtml from 'draftjs-to-html';
 import { convertToRaw } from 'draft-js';
-import {  ContentState, convertFromHTML } from 'draft-js'
+import { ContentState, convertFromHTML } from 'draft-js'
 import Swal from "sweetalert2"
 import htmlToDraft from 'html-to-draftjs';
 
 
-const estilos=makeStyles((theme)=>({
+const estilos = makeStyles((theme) => ({
 
-    divTituloGeneral:{
-        margin:"55px auto",
-        width:"85vw",
-        " & .MuiButton-containedPrimary:hover" :{
+    divTituloGeneral: {
+        margin: "55px auto",
+        width: "85vw",
+        " & .MuiButton-containedPrimary:hover": {
             backgroundColor: "#303f9f00",
-            color:"#5fcccf",
-            border:"1px solid"
+            color: "#5fcccf",
+            border: "1px solid"
         },
 
-},
-tituloGeneral:{
-
-    fontWeight:"700",
-    fontFamily:"Lato",
-    fontSize:"27px"
     },
-    divCircular:{
-        marginTop:"200px"
+    tituloGeneral: {
 
+        fontWeight: "700",
+        fontFamily: "Lato",
+        fontSize: "27px"
     },
+    divCircular: {
+        marginTop: "200px"
 
-margenDiv:{
-    margin:"100px 0px 0px 0px"
-},
-circular:{
-    height:"72px !important",
-    width:"72px !important",
-    display:"block",
-    margin:"auto"
     },
 
-    margenDivContenedor:{
-        margin:"30px 0px"
+    margenDiv: {
+        margin: "100px 0px 0px 0px"
     },
-    textoTitulo:{
-        margin:"0px 0px 10px 5px"
+    circular: {
+        height: "72px !important",
+        width: "72px !important",
+        display: "block",
+        margin: "auto"
+    },
+
+    margenDivContenedor: {
+        margin: "30px 0px"
+    },
+    textoTitulo: {
+        margin: "0px 0px 10px 5px"
     }
 
 }))
@@ -55,18 +55,18 @@ circular:{
 
 
 const AdministrarNosotros = (props) => {
-    const clases=estilos()
+    const clases = estilos()
     const [titulo, settitulo] = useState("")
     const [editorState, setEditorState] = useState(EditorState.createWithContent(
         ContentState.createFromBlockArray(
-          convertFromHTML('<p></p>')
+            convertFromHTML('<p></p>')
         )
-      ))
-   //   const [editorState, setEditorState] = useState(EditorState.createEmpty())
+    ))
+    //   const [editorState, setEditorState] = useState(EditorState.createEmpty())
 
     const [cargando, setcargando] = useState(false)
 
-  
+
 
 
 
@@ -76,10 +76,10 @@ const AdministrarNosotros = (props) => {
 
     const onEditorStateChange = (editorState) => {
         setEditorState(editorState)
-      }
-    
+    }
 
-    const onTitleStateChange=(e)=>{
+
+    const onTitleStateChange = (e) => {
         e.preventDefault()
         settitulo(e.target.value)
     }
@@ -87,49 +87,49 @@ const AdministrarNosotros = (props) => {
 
 
 
-const remplazarTexto =(htmlText)=>{
-  console.log(htmlText)
+    const remplazarTexto = (htmlText) => {
+        console.log(htmlText)
 
-  //<div style="text-align:none;">
- let textoprueba=htmlText.replace(`<div style="text-align:none;">`,`<div style="text-align: center; width: 100%;">`)
+        //<div style="text-align:none;">
+        let textoprueba = htmlText.replace(`<div style="text-align:none;">`, `<div style="text-align: center; width: 100%;">`)
 
- 
- return textoprueba
-}
 
-    const Guardar=()=>{
+        return textoprueba
+    }
+
+    const Guardar = () => {
 
         const rawContentState = convertToRaw(editorState.getCurrentContent());
         let htmlText = draftToHtml(rawContentState);
         let htmlConverted = remplazarTexto(htmlText) //here I call the function that converts the <img/> to <div><img/></div>. 
-        
 
-        const configWeb={
-            title:titulo,
+
+        const configWeb = {
+            title: titulo,
             //contenido:draftToHtml(convertToRaw(editorState.getCurrentContent()))
-            contenido:htmlConverted
+            contenido: htmlConverted
         }
 
 
 
-        Firebase.db.collection("administrarnosotros").doc("configuracion").set(configWeb).then(()=>
-        
-         Swal.fire({
-             title:"Su informacion ha sido guardada",
-             icon:"success",
-             showConfirmButton: false,
-             timer:1500
-         }).then(()=>{
-             props.history.push("/nosotros")
-         })
-        ).catch(error=>console.log(error))
+        Firebase.db.collection("administrarnosotros").doc("configuracion").set(configWeb).then(() =>
+
+            Swal.fire({
+                title: "Su informacion ha sido guardada",
+                icon: "success",
+                showConfirmButton: false,
+                timer: 1500
+            }).then(() => {
+                props.history.push("/nosotros")
+            })
+        ).catch(error => console.log(error))
 
 
         //guardar en la base de datos
 
 
     }
-  
+
 
 
 
@@ -138,35 +138,35 @@ const remplazarTexto =(htmlText)=>{
     useEffect(async () => {
 
         setcargando(true)
-    
-        const retornarObjeto=await Firebase.db.collection("administrarnosotros").doc("configuracion").get().then((respuesta)=>{
+
+        const retornarObjeto = await Firebase.db.collection("administrarnosotros").doc("configuracion").get().then((respuesta) => {
             console.log(respuesta.data())
 
             return respuesta.data()
         })
 
-        if(retornarObjeto!=undefined){
-            
+        if (retornarObjeto != undefined) {
+
             settitulo(retornarObjeto.title)
 
-// funcion extraida 
-           // const blocksFromHTML = convertFromHTML(retornarObjeto.contenido);
-           
+            // funcion extraida 
+            // const blocksFromHTML = convertFromHTML(retornarObjeto.contenido);
+
             const contentBlock = htmlToDraft(retornarObjeto.contenido);
             const contentState = ContentState.createFromBlockArray(contentBlock.contentBlocks);
             // const text = ContentState.createFromBlockArray(
             //         blocksFromHTML.contentBlocks,
             //         blocksFromHTML.entityMap
             //         );
-                    
-                    const editorState = EditorState.createWithContent(contentState);
-                
-                setEditorState(
-                    editorState 
+
+            const editorState = EditorState.createWithContent(contentState);
+
+            setEditorState(
+                editorState
             )
 
 
-// fin de la funcion extraida 
+            // fin de la funcion extraida 
             // setEditorState(EditorState.createWithContent(
             //     ContentState.createFromBlockArray(
             //       convertFromHTML(retornarObjeto.contenido)
@@ -184,68 +184,68 @@ const remplazarTexto =(htmlText)=>{
 
 
     return (
-        
 
-<div className={clases.divTituloGeneral}>
-<Typography variant="h4" className={clases.tituloGeneral}>
-Administrar Paginas Nosotros
-</Typography>
 
-<div className={clases.margenDiv}>
+        <div className={clases.divTituloGeneral}>
+            <Typography variant="h4" className={clases.tituloGeneral}>
+                Administrar Paginas Nosotros
+            </Typography>
 
-{
-cargando?
-                            <div className={clases.divCircular} >
+            <div className={clases.margenDiv}>
+
+                {
+                    cargando ?
+                        <div className={clases.divCircular} >
                             <CircularProgress className={clases.circular}></CircularProgress>
                         </div>
-                        :<div>
-                          
- <Grid container>
+                        : <div>
 
-<Grid xs={1} ></Grid>
-<Grid xs={10}>
-            <Grid xs={12} md={12}>
+                            <Grid container>
 
-            <div className={clases.margenDivContenedor}>
-                    <Typography className={clases.textoTitulo} variant="h6" >
-                        Titulo
-                    </Typography>
-                    <TextField value={titulo} onChange={onTitleStateChange} id="outlined-basic"  variant="outlined" fullWidth />
+                                <Grid xs={1} ></Grid>
+                                <Grid xs={10}>
+                                    <Grid xs={12} md={12}>
+
+                                        <div className={clases.margenDivContenedor}>
+                                            <Typography className={clases.textoTitulo} variant="h6" >
+                                                Titulo
+                                            </Typography>
+                                            <TextField value={titulo} onChange={onTitleStateChange} id="outlined-basic" variant="outlined" fullWidth />
+                                        </div>
+
+                                        <div className={clases.margenDivContenedor}>
+                                            <Typography className={clases.textoTitulo} variant="h6" >
+                                                Contenido
+                                            </Typography>
+                                            <TextoEditor editorState={editorState} onEditorStateChange={setEditorState} ></TextoEditor>
+
+                                        </div>
+
+                                        <Button onClick={() => Guardar()} variant="contained" color="primary">
+                                            Guardar
+                                        </Button>
+
+                                    </Grid>
+
+
+
+
+
+                                </Grid>
+                                <Grid xs={1} ></Grid>
+
+
+                            </Grid>
+                        </div>
+
+
+
+                }
+
+
+
             </div>
-
-            <div className={clases.margenDivContenedor}>
-                    <Typography className={clases.textoTitulo} variant="h6" >
-                        Contenido
-                    </Typography>
-            <TextoEditor editorState={editorState} onEditorStateChange={setEditorState} ></TextoEditor>
-            
-            </div>
-
-            <Button onClick={()=>Guardar()} variant="contained" color="primary">
-                Guardar
-            </Button>
-
-            </Grid>
-
-
-
-    
-
-</Grid>
-<Grid xs={1} ></Grid>
-
-
-</Grid>    
-</div>
-
-
-     
-}                          
-
-
-
-</div>
-</div>
+        </div>
     )
 }
 
